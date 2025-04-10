57f29cc8302a8ba4ae1bf5171e341dbd7db4d4c9
TUF <TUF@DESKTOP-95RODFH>
2025-04-10 18:26:32 +0530

    delete metthod

---
+++ Selenium_aid/src/test/java/rest_api_testing/Delete_API_Method.java
@@ -0,0 +1,20 @@
+package rest_api_testing;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Delete_API_Method {
+
+	public static void main(String[] args) {
+Response deleteReq=RestAssured.delete("");
+int statusCode=deleteReq.getStatusCode();
+long time=deleteReq.getTime();
+String body=deleteReq.asString();
+
+System.out.println(statusCode);
+System.out.println(time);
+System.out.println(body);
+
+	}
+
+}
